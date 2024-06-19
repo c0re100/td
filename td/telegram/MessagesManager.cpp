ee41d2fb12e08a0122abef18e6e8dcb5d24efaa1
c0re100 <corehusky@gmail.com>
2024-06-19 14:38:19 +0800

    Fix

--- td/telegram/MessagesManager.cpp
+++ td/telegram/MessagesManager.cpp
@@ -23259,7 +23259,7 @@ MessageInputReplyTo MessagesManager::create_message_input_reply_to(
 
         // TODO local replies to local messages can be allowed
         // TODO replies to yet unsent messages can be allowed with special handling of them on application restart
-        return MessageInputReplyTo{message_id, reply_dialog_id, MessageQuote{td_, std::move(reply_to_message->quote_)}};
+        return MessageInputReplyTo{message_id, DialogId(), MessageQuote{td_, std::move(reply_to_message->quote_)}};
       }
       return MessageInputReplyTo{m->message_id, DialogId(), MessageQuote{td_, std::move(reply_to_message->quote_)}};
     }
